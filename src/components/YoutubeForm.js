f474e6e1f1dacf2f8561c719d01b17fafed0abd3
codecamjam <camerontaghlabi@gmail.com>
2022-05-20 19:51:57 -0400

    l14 - form component

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,4 +1,4 @@
-import { Formik } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
 const initialValues = {
@@ -24,7 +24,7 @@ function YoutubeForm() {
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      <form onSubmit={formik.handleSubmit}>
+      <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <input
@@ -65,7 +65,7 @@ function YoutubeForm() {
         </div>
 
         <button type="submit">Submit</button>
-      </form>
+      </Form>
     </Formik>
   );
 }
