e58f61ff7fcade3f40498578a00c301e0bc491e9
codecamjam <camerontaghlabi@gmail.com>
2022-05-20 20:37:49 -0400

    l21 - arrays

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -12,6 +12,7 @@ const initialValues = {
     facebook: '',
     twitter: '',
   },
+  phoneNumbers: ['', ''],
 };
 
 const onSubmit = (values) => {
@@ -83,9 +84,20 @@ function YoutubeForm() {
           <label htmlFor="facebook">Facebook profile</label>
           <Field type="text" id="facebook" name="social.facebook" />
         </div>
+
         <div className="form-control">
           <label htmlFor="twitter">Twitter profile</label>
           <Field type="text" id="twitter" name="social.twitter" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="primaryPh">Primary phone number</label>
+          <Field type="text" id="primaryPh" name="phoneNumbers[0]" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="secondaryPh">Secondary phone number</label>
+          <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
         </div>
 
         <button type="submit">Submit</button>
