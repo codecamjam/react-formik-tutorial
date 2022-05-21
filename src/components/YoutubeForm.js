b3ecf386b8351884ed20868ab5678f0a550b8f8d
codecamjam <camerontaghlabi@gmail.com>
2022-05-20 20:32:10 -0400

    l20 - nested objects

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -8,6 +8,10 @@ const initialValues = {
   channel: '',
   comments: '',
   address: '',
+  social: {
+    facebook: '',
+    twitter: '',
+  },
 };
 
 const onSubmit = (values) => {
@@ -73,6 +77,15 @@ function YoutubeForm() {
               );
             }}
           </Field>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook profile</label>
+          <Field type="text" id="facebook" name="social.facebook" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter profile</label>
+          <Field type="text" id="twitter" name="social.twitter" />
         </div>
 
         <button type="submit">Submit</button>
